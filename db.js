3c33b521a07fe44da43dfbb7c85cc4ecb21238a3
pkiop <pkiopb@gmail.com>
2022-09-29 21:49:12 +0900

    Chore: add .env

--- db.js
+++ db.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose");
-const uri = "mongodb+srv://StockReview:AstonMartinDB12@cluster0.jiyst.mongodb.net/stockreview?retryWrites=true&w=majority";
+const mongoose = require('mongoose');
+const uri = process.env.DB_URL;
 
 const connectDB = async () => {
-    await mongoose.connect (uri, {
-        useUnifiedTopology : true,
-        useNewUrlParser : true
-    })
-    console.log("mongodb has successfully connected");
-
-}
+  await mongoose.connect(uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  console.log('mongodb has successfully connected');
+};
 
 module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const bodyParser = require("body-parser");
-const investor = require("./routes/api/investor");
-const post = require("./routes/api/post");
-const admin = require("./routes/api/admin");
-const connectDB = require("./db");
+const express = require('express');
+const morgan = require('morgan');
+require('dotenv').config();
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const investor = require('./routes/api/investor');
+const post = require('./routes/api/post');
+const admin = require('./routes/api/admin');
+const connectDB = require('./db');
 
 const app = express();
 const port = 4000;
@@ -14,21 +15,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "POST, GET, PUT, OPTIONS, DELETE"
+    'Access-Control-Allow-Methods',
+    'POST, GET, PUT, OPTIONS, DELETE'
   );
   next();
 });
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(helmet());
 connectDB();
 
-app.use("/api/v1/post", post);
-app.use("/api/v1/investor", investor);
-app.use("/api/v1/admin", admin);
+app.use('/api/v1/post', post);
+app.use('/api/v1/investor', investor);
+app.use('/api/v1/admin', admin);
 
 app.listen(port, () => console.log(`API Server Listening on port ${port}`));
