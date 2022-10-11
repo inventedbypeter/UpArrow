7f1b782bda3bb3bb91f7e61fe7ef7f39b4b798da
pkiop <pkiopb@gmail.com>
2022-10-11 22:44:16 +0900

    fix type

--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-  
+
 const CommentSchema = new Schema({
-    stockId: String,
-    userId: String,
-    postId: String,
-    content: String,
-    timeStamp: String,
-    likes: Array
-})
+  stockId: String,
+  userId: String,
+  postId: String,
+  content: String,
+  timeStamp: Date,
+  likes: Array,
+});
 
 module.exports = mongoose.model('Comment', CommentSchema);
